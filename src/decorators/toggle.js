1c8151698388350de249eb471052ed60d420e680
Patrick Fowler <pfowler@cs.stanford.edu>
2018-05-02 09:41:39 -0700

    Add deprecation warning to the @toggle method, and restore peer dependencies of mobx 2.5 and 3

--- src/decorators/toggle.js
+++ src/decorators/toggle.js
@@ -1,6 +1,7 @@
 import { action } from 'mobx';
+import { deprecated } from 'deprecated';
 
-export function toggle(...args) {
+function toggleFn(...args) {
   const fnName = args[0] || 'active';
   const propKey = args[1] || 'isActive';
   return action((target) => {
@@ -9,8 +10,18 @@ export function toggle(...args) {
       [fnName]: action((flag = null) => {
         if (flag === true) return Object.assign(target.prototype, { [propKey]: true });
         if (flag === false) return Object.assign(target.prototype, { [propKey]: false });
-        return Object.assign(target.prototype, { [propKey]: !target.prototype[propKey] });
+        return Object.assign(target.prototype, {
+          [propKey]: !target.prototype[propKey],
+        });
       }),
     });
   });
 }
+
+const toggle = deprecated.method(
+  'The `@toggle` decorator is incompatible with mobx 4 and has been deprecated. It will be removed in the next major version.',
+  console.error, // eslint-disable-line no-console
+  toggleFn,
+);
+
+export default toggle;
